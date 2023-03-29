fe7cb6534464841698f2bf7ff1c015ced2bff5e5
pl251351 <pl251351@ahschool.com>
2023-03-29 08:19:27 -0400

    addded michael's image and tested a bunch of random stuff

--- turning_detection_2.py
+++ turning_detection_2.py
@@ -1,7 +1,5 @@
 ###https://stackoverflow.com/questions/49037902/how-to-interpolate-a-line-between-two-other-lines-in-python
 ###https://stackoverflow.com/questions/69233701/finding-the-coordinates-of-pixels-over-a-line-in-an-image
-<<<<<<< HEAD
+
 ###https://en.wikipedia.org/wiki/Generalised_Hough_transform
-=======
->>>>>>> origin/master
 

24c785dbc4c94f538e7787bdbab3d2d47a3ff613
Sankalp Singh <838sanky@gmail.com>
2023-03-29 19:37:10 -0400

    Program sort of works? Have to make sure the drawing is perfect before testing and changing code too much.

--- turning_detection.py
+++ turning_detection.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-####https://docs.opencv.org/3.4/da/ddc/tutorial_generalized_hough_ballard_guil.html
+
 # Load the image
 img = cv2.imread('turning_pictures/turning_curved_3.png')
 
@@ -11,49 +11,76 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray, 50, 150, apertureSize=3)
 
 # Apply HoughLinesP transform to detect lines
-lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi / 180, threshold=30, minLineLength=500, maxLineGap=50)
+lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi / 180, threshold=30, minLineLength=20, maxLineGap=10)
 
-# Maths :(
 if lines is not None:
-    # Calculate the center of the two longest lines
-    longest_lines = sorted(lines, key=lambda x: x[0][2] - x[0][0], reverse=True)[:2]
-    x1, y1, x2, y2 = longest_lines[0][0]
-    x3, y3, x4, y4 = longest_lines[1][0]
-    center_x = int((x1 + x2 + x3 + x4) / 4)
-    center_y = int((y1 + y2 + y3 + y4) / 4)
-    print("Center line coordinate point 1: ({}, {})".format(center_x, center_y))
-    print(x1, x2, y1, y2)
-    # Find the slope of one of the lines
-    m = (y2 - y1) / (x2 - x1)
-    # Find the y-intercept of the center line using the center point as input to determine the y-intercept of the
-    # center line since they are parallel. Parallel means same slope different y-intercept.
-    c = center_y - (m * center_x)
-    # Find the final y values of the two center line points
-    final_y1 = int((m * 10000) + c)
-    final_y2 = int((m * -10000) + c)
-    # Find the final y values of the two arrow line points
-    arrow_final_y1 = int((m * 900) + c)
-    arrow_final_y2 = int((m * 800) + c)
-    # Create the coordinates for the center line points
-    p1 = (10000, final_y1)
-    p2 = (-10000, final_y2)
-    # Create the coordinates for the arrow line points
-    pa1 = (900, arrow_final_y1)
-    pa2 = (800, arrow_final_y2)
+    # Create empty arrays to store the endpoints of the left and right curves
+    left_endpoints = []
+    right_endpoints = []
 
-    # Draw a red center point, center line, and direction of travel
-    cv2.circle(img, (center_x, center_y), 5, (0, 0, 255), -1)
-    cv2.line(img, p1, p2, (255, 0, 255), 2)
-    cv2.arrowedLine(img, pa1, pa2, (0, 0, 255), 10)
-    cv2.circle(img, (0, 50), 30, (0, 0, 255), -1)
-
-# Draw the detected lines on the image
-if lines is not None:
+    # Iterate through each line detected by HoughLinesP
     for line in lines:
         x1, y1, x2, y2 = line[0]
-        cv2.line(img, (x1, y1), (x2, y2), (255, 255, 0), 2)
+        # Calculate the slope of the line
+        slope = (y2 - y1) / (x2 - x1) if x2 != x1 else 0
 
-# Display the image
-cv2.imshow("Image", img)
+        # Check if the slope is within a range corresponding to a curved line
+        if slope > 0.5:
+            right_endpoints.append((x1, y1))
+            right_endpoints.append((x2, y2))
+        elif slope < -0.5:
+            left_endpoints.append((x1, y1))
+            left_endpoints.append((x2, y2))
+
+    # Create arrays of x and y coordinates for each set of endpoints
+    left_x = [point[0] for point in left_endpoints]
+    left_y = [point[1] for point in left_endpoints]
+    right_x = [point[0] for point in right_endpoints]
+    right_y = [point[1] for point in right_endpoints]
+
+    if len(left_x) > 0 and len(left_y) > 0:
+        # Fit a polynomial curve to the points using numpy's polyfit function
+        left_fit = np.polyfit(left_y, left_x, 2)
+
+        # Generate x and y values for plotting the curve
+        plot_y = np.linspace(0, img.shape[0] - 1, img.shape[0])
+        left_fit_x = left_fit[0] * plot_y ** 2 + left_fit[1] * plot_y + left_fit[2]
+
+        # Convert the x and y coordinates of the curve to integer values
+        left_fit_x = np.array(left_fit_x, np.int32)
+        plot_y = np.array(plot_y, np.int32)
+
+        # Reshape the x and y coordinates into a format required by cv2.polylines
+        left_points = np.array([np.transpose(np.vstack([left_fit_x, plot_y]))])
+
+        # Draw the left curve on the image
+        cv2.polylines(img, [left_points], False, (255, 0, 0), 5)
+    else:
+        print("No left curve detected.")
+
+    if len(right_x) > 0 and len(right_y) > 0:
+        # Fit a polynomial curve to the points using numpy's polyfit function
+        right_fit = np.polyfit(right_y, right_x, 2)
+
+        # Generate x and y values for plotting the curve
+        plot_y = np.linspace(0, img.shape[0] - 1, img.shape[0])
+        right_fit_x = right_fit[0] * plot_y ** 2 + right_fit[1] * plot_y + right_fit[2]
+
+        # Convert the x and y coordinates of the curve to integer values
+        right_fit_x = np.array(right_fit_x, np.int32)
+        plot_y = np.array(plot_y, np.int32)
+
+        # Reshape the x and y coordinates into a format required by cv2.polylines
+        right_points = np.array([np.transpose(np.vstack([right_fit_x, plot_y]))])
+
+        # Draw the right curve on the image
+        cv2.polylines(img, [right_points], False, (255, 0, 0), 5)
+    else:
+        print("No right curve detected.")
+
+# Display the image with curves drawn on it
+cv2.imshow('Curves Detected', img)
+
+# Wait for user input and then close the window
 cv2.waitKey(0)
 cv2.destroyAllWindows()

b84c22a343216f2a2a36f5b653ee43de926d3ed3
Sankalp Singh <838sanky@gmail.com>
2023-04-03 23:55:41 -0400

    Contours testing added

--- TurningDetectionWorking.py
+++ TurningDetectionWorking.py
@@ -17,39 +17,45 @@ lines = cv2.HoughLinesP(
     edges,  # Input edge image
     1,  # Distance resolution in pixels
     np.pi / 180,  # Angle resolution in radians
-    threshold=0,  # Min number of votes for valid line
+    threshold=5,  # Min number of votes for valid line
     minLineLength=0,  # Min allowed length of line
-    maxLineGap=10  # Max allowed gap between line for joining them
+    maxLineGap=0  # Max allowed gap between line for joining them
 )
 
 # Iterate over points
 for points in lines:
     # Extracted points nested in the list
     x1, y1, x2, y2 = points[0]
-    # Draw the lines joing the points
-    # On the original image
+    # Draw the lines join the points on the original image
     cv2.line(image, (x1, y1), (x2, y2), (255, 0, 255), 2)
+    # cv2.putText(image, "(%d, %d)" % (x1, y1), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
+    # cv2.putText(image, "(%d, %d)" % (x2, y2), (x2, y2), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
     # Maintain a simples lookup list for points
     lines_list.append([(x1, y1), (x2, y2)])
 
-longest_lines = sorted(lines, key=lambda x: x[0][2] - x[0][0], reverse=True)[:10]
-x1, y1, x2, y2 = longest_lines[0][0]
-x3, y3, x4, y4 = longest_lines[1][0]
-center_x = int((x1 + x2 + x3 + x4) / 4)
-center_y = int((y1 + y2 + y3 + y4) / 4)
-print("Center line coordinate point 1: ({}, {})".format(center_x, center_y))
-print(x1, x2, y1, y2)
-cv2.circle(image, (center_x, center_y), 5, (0, 0, 255), -1)
-# x12, y12, x22, y22 = longest_lines[4][0]
-# x32, y32, x42, y42 = longest_lines[5][0]
-# center_x2 = int((x12 + x22 + x32 + x42) / 4)
-# center_y2 = int((y12 + y22 + y32 + y42) / 4)
-# print("Center line coordinate point 2: ({}, {})".format(center_x2, center_y2))
-# print(x12, x22, y12, y22)
-# cv2.circle(image, (671, center_y2), 5, (0, 0, 255), -1)
+print(lines_list)
+# Calculate the total sum of x-values and y-values
+# sum_x = 0
+# sum_y = 0
+# count = 0
+#
+# for pair in lines_list:
+#     for point in pair:
+#         sum_x += point[0]
+#         sum_y += point[1]
+#         count += 1
+#
+# # Calculate the average of x-values and y-values
+# avg_x = sum_x / count
+# avg_y = sum_y / count
+# center_coordinates = (avg_x, avg_y)
+# # Print the average coordinates
+# print(f"The average x-coordinate is {avg_x:.2f}")
+# print(f"The average y-coordinate is {avg_y:.2f}")
+#
+# cv2.circle(image, (891,281), 5, (0, 0, 255), 5)
 
-
-cv2.imshow("Image", image)
+img_resized = cv2.resize(image, (1000, 1000))
+cv2.imshow("Curves Detected With Midline", img_resized)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
--- straight_line_detection.py
+++ straight_line_detection.py
@@ -52,6 +52,7 @@ if lines is not None:
         x1, y1, x2, y2 = line[0]
         cv2.line(img, (x1, y1), (x2, y2), (255, 255, 0), 2)
 
+
 # Display the image
 cv2.imshow("Image", img)
 cv2.waitKey(0)

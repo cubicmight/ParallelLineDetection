d14042e3675709ff06c16a1a3ccc7c3566bd6e72
Sankalp Singh <838sanky@gmail.com>
2023-05-21 13:46:58 -0400

    lane detection + center line

--- lane_detection.py
+++ lane_detection.py
@@ -1,41 +1,106 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
-# Step 1: Importing Libraries
-image = cv2.imread('test_images/01. solidWhiteCurve.jpg')
 
-# Step 2: Reading the Image
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+def region_of_interest(img, vertices):
+    """
+    Applies a region of interest mask to the image.
+    """
+    mask = np.zeros_like(img)
+    cv2.fillPoly(mask, vertices, 255)
+    masked_img = cv2.bitwise_and(img, mask)
+    return masked_img
 
-# Step 3: Converting to Grayscale
-blur = cv2.GaussianBlur(gray, (5, 5), 0)
 
-# Step 4: Gaussian Blur
-edges = cv2.Canny(blur, 50, 150)
+def draw_lines(img, lines, color=(0, 255, 0), thickness=3):
+    """
+    Draws lines on the image.
+    """
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            cv2.line(img, (x1, y1), (x2, y2), color, thickness)
 
-# Step 5: Canny Edge Detection
-height, width = image.shape[:2]
-roi_vertices = [(0, height), (width/2, height/2), (width, height)]
-mask_color = 255
-mask = np.zeros_like(edges)
-cv2.fillPoly(mask, np.array([roi_vertices], dtype=np.int32), mask_color)
-masked_edges = cv2.bitwise_and(edges, mask)
 
-# Step 6: Region of Interest
-lines = cv2.HoughLinesP(masked_edges, rho=6, theta=np.pi/60, threshold=160, minLineLength=40, maxLineGap=25)
+def detect_lanes(img):
+    """
+    Detects lanes in the image.
+    """
+    # Convert the image to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Step 7: Hough Transform
-line_image = np.zeros_like(image)
-for line in lines:
-    x1, y1, x2, y2 = line[0]
-    cv2.line(line_image, (x1, y1), (x2, y2), (0, 255, 0), 5)
+    # Apply Gaussian blur to reduce noise
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
 
-# Step 8: Drawing the Lines
-final_image = cv2.addWeighted(image, 0.8, line_image, 1, 0)
+    # Perform edge detection using the Canny algorithm
+    edges = cv2.Canny(blur, 50, 150)
 
-# Step 9: Overlaying the Lines on the Original Image
-plt.imshow(final_image)
+    # Define a region of interest (ROI) for lane detection
+    height, width = edges.shape[:2]
+    roi_vertices = np.array([[(0, height), (width / 2, height / 2), (width, height)]], dtype=np.int32)
+    roi = region_of_interest(edges, roi_vertices)
 
-# Step 10: Display Image
-plt.show()
+    # Perform Hough line detection to find lane lines
+    lines = cv2.HoughLinesP(roi, rho=1, theta=np.pi / 180, threshold=30, minLineLength=20, maxLineGap=100)
+
+    # Create an empty image to draw the lane lines
+    lane_lines_img = np.zeros_like(img)
+
+    # Calculate the center line position
+    center_x = width / 2
+
+    # Define the maximum y-coordinate for the lane lines to be drawn
+    max_y = int(height * 0.6)
+
+    # Draw the lane lines
+    left_lane_lines = []
+    right_lane_lines = []
+
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            # Calculate the slope of the line
+            slope = (y2 - y1) / (x2 - x1)
+
+            # Classify the line as left or right based on the slope and its position relative to the center
+            if slope < -0.2 and x1 < center_x and x2 < center_x:
+                left_lane_lines.append(line)
+            elif slope > 0.2 and x1 > center_x and x2 > center_x:
+                right_lane_lines.append(line)
+
+    # Draw the left lane lines
+    draw_lines(lane_lines_img, left_lane_lines, color=(0, 0, 255))
+
+    # Draw the right lane lines
+    draw_lines(lane_lines_img, right_lane_lines, color=(0, 0, 255))
+
+    # Draw the center line up to the maximum y-coordinate
+    cv2.line(lane_lines_img, (int(center_x), height), (int(center_x), max_y), color=(0, 255, 0), thickness=3)
+
+    # Combine the lane lines image with the original image
+    output = cv2.addWeighted(img, 0.8, lane_lines_img, 1, 0)
+
+    return output
+
+
+# Load video file
+video_path = 'videos/solidYellowLeft.mp4'
+cap = cv2.VideoCapture(video_path)
+
+# Loop through each frame in the video
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    # Detect lanes in the frame
+    lanes_frame = detect_lanes(frame)
+
+    # Display the resulting frame
+    cv2.imshow('Lane Detection', lanes_frame)
+
+    # Exit loop when 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release resources
+cap.release()
+cv2.destroyAllWindows()
